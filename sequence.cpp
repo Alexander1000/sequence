eefa9d8151a6fe2a58180951c41810de492bab91
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2016-06-12 18:22:51 +0300

    Add memcpy

--- sequence.cpp
+++ sequence.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <memory.h>
+
+#define SIZE_INT sizeof(int)
 
 using namespace std;
 
@@ -41,28 +44,21 @@ class SequenceGenerator
 	        }
 
 	        for (int i = 0; i < size; ++i) {
-	            int row = digits[i];
+	            int *sourceNext = (int*) malloc((this->size - size + 1) * SIZE_INT);
+	            int *distNext = (int*) malloc((size - 1) * SIZE_INT);
 
-	            int *sourceNext = (int*) malloc(sizeof(int) * (this->size - size + 1));
-	            int *distNext = (int*) malloc(sizeof(int) * (size - 1));
-
-	            for (int j = 0; j < this->size - size; ++j) {
-	                sourceNext[j] = source[j];
-	            }
-
-	            sourceNext[this->size - size] = row;
-
-	            for (int j = 0; j < size; ++j) {
-	                if (i > j) {
-	                    distNext[j] = digits[j];
-	                }
-
-	                if (i < j) {
-	                    distNext[j - 1] = digits[j];
-	                }
-	            }
+				memcpy(sourceNext, source, (this->size - size) * SIZE_INT);
+				memcpy(sourceNext + (this->size - size), digits + i, SIZE_INT);
+				
+				memcpy(distNext, digits, i * SIZE_INT);
+				memcpy(distNext + i, digits + (i + 1), (size - i - 1) * SIZE_INT);
 
 	            this->genSeq(sourceNext, distNext, size - 1);
+
+				if (size > 1) {
+					free(sourceNext);
+					free(distNext);
+				}
 	        }
 	    };
 
@@ -75,7 +71,6 @@ class SequenceGenerator
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
 		int *digits = (int*) malloc((argc - 1) * sizeof(int));
-		memset(digits, 0, (argc - 1) * sizeof(int));
 
 		for (int i = 0; i < argc - 1; ++i) {
 		    digits[i] = atoi(argv[i + 1]);
@@ -91,7 +86,10 @@ int main(int argc, char* argv[]) {
 		    }
 
 		    cout << "\n";
+			free(map[i]);
 		}
+
+		free(map);
 	} else {
 		cout << "Not ok\n";
 	}
