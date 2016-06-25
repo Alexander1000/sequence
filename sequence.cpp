e82bfb00e5f15f4b8658e5f82a8e7f47f7ac972a
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2016-06-25 13:37:53 +0300

    Add -w flag

--- sequence.cpp
+++ sequence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 
 #define SIZE_INT sizeof(int)
 
@@ -28,28 +29,73 @@ class SequenceGenerator
             return this->result;
         };
 
+		void setWidth(int width) {
+			this->width = width;
+		}
+
+		int getCurrentPosition() {
+			return this->curPosition;
+		}
+
         int sizeResult;
 	private:
+		/**
+		 * source - текущая последовательность
+		 * digits - набор элементов для последовательности
+		 * size - количество элементов в digits
+		 */
 	    void genSeq(int *source, int *digits, int size) {
-	        if (size == 0) {
+			// длина текущей последовательности
+			int sourceLength = this->size - size;
+
+	        if (size == 0 || (this->width > 0 && sourceLength == this->width)) {
+				// количество сравниваемых элементов
+				int cmpSize = (this->width > 0 ? this->width : this->size);
+
+				if (this->curPosition > 0) {
+					bool test = true;
+
+					for (int i = 0; i < cmpSize; ++i) {
+						if (this->result[this->curPosition - 1][i] != source[i]) {
+							test = false;
+							break;
+						}
+					}
+
+					if (test) {
+						return;
+					}
+				}
+
 	            this->result[this->curPosition] = source;
 	            this->curPosition++;
 	            return;
 	        }
 
 	        for (int i = 0; i < size; ++i) {
-	            int *sourceNext = (int*) malloc((this->size - size + 1) * SIZE_INT);
+	            int *sourceNext = (int*) malloc((sourceLength + 1) * SIZE_INT);
 	            int *distNext = (int*) malloc((size - 1) * SIZE_INT);
 
-				memcpy(sourceNext, source, (this->size - size) * SIZE_INT);
-				memcpy(sourceNext + (this->size - size), digits + i, SIZE_INT);
+				memcpy(sourceNext, source, sourceLength * SIZE_INT);
+				memcpy(sourceNext + sourceLength, digits + i, SIZE_INT);
 				
 				memcpy(distNext, digits, i * SIZE_INT);
 				memcpy(distNext + i, digits + (i + 1), (size - i - 1) * SIZE_INT);
 
-	            this->genSeq(sourceNext, distNext, size - 1);
+				if (size > 0 && (this->width <= 0 || (this->width > 0 && sourceLength < this->width))) {
+					/**
+					 * Продолжаем генерировать последовательность когда:
+					 * 1) Есть еще данные для последовательности (size > 0)
+					 * AND
+					 * 2.1) Ограничитель длины не установлен (this->width <= 0)
+					 * OR
+					 * 2.2.1) Установлен ограничитель длины (this->width > 0)
+					 * 2.2.2) Построенная последовательность все еще меньше ограничителя (sourceLength < this->width)
+					 */
+	            	this->genSeq(sourceNext, distNext, size - 1);
+				}
 
-				if (size > 1) {
+				if (size > 1 && (this->width <= 0 || (this->width > 0 && sourceLength + 1 < this->width))) {
 					free(sourceNext);
 					free(distNext);
 				}
@@ -60,25 +106,39 @@ class SequenceGenerator
 		int *digits;
 		int curPosition;
 		int **result;
+		int width;
 };
 
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
-		int *digits = (int*) malloc((argc - 1) * SIZE_INT);
+		char* firstVar = argv[1];
+		int offset = 1;
+		int width = 0;
 
-		for (int i = 0; i < argc - 1; ++i) {
-		    digits[i] = atoi(argv[i + 1]);
+		// -w (width)
+		if (strlen(firstVar) == 2 && argc > 3 && (int) firstVar[0] == 45 && (int) firstVar[1] == 119) {
+			width = atoi(argv[2]);
+			offset += 2;
 		}
 
-		SequenceGenerator seq(digits, argc - 1);
+		int *digits = (int*) malloc((argc - offset) * SIZE_INT);
+
+		for (int i = 0; i < argc - offset; ++i) {
+		    digits[i] = atoi(argv[i + offset]);
+		}
+
+		SequenceGenerator seq(digits, argc - offset);
+		seq.setWidth(width);
 
 		int **map = seq.generate();
 
-		for (int i = 0; i < seq.sizeResult; ++i) {
-		    for (int j = 0; j < argc - 1; ++j) {
+		int sizeDataSet = (width > 0 ? width : argc - offset);
+
+		for (int i = 0; i < seq.getCurrentPosition(); ++i) {
+		    for (int j = 0; j < sizeDataSet; ++j) {
 		        cout << map[i][j];
 		        
-		        if (j != argc - 2) {
+		        if (j != argc - offset - 1) {
 		        	cout << " ";
 				}
 		    }
